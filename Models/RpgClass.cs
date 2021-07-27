83815e088ea58c91b2717e8a8bbf1b13e9bd0068
tarunparmar <tarunparmar@gmail.com>
2021-07-27 17:40:21 -0500

    added http methods, tried attribute routing & more

---
+++ Models/RpgClass.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight,
+
+        Mage,
+
+        Cleric
+    }
+}
